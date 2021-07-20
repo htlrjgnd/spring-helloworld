69f0c9ea21c97554440b3c78d1720aca84337626
Dmitriy Tyavin <tyav89@yandex.ru>
2021-07-20 13:06:45 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean.equals(bean2));
+        System.out.println(catBean1.equals(catBean2));
     }
 }
